0ecba3f85b15c9887e312ea1208127d43275fb60
wardxela <wardxela@gmail.com>
2026-09-04 17:51:52 +0300

    musicbrainz: configure User Agent

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use dismus::fs_library::FileSystemLibrary;
-use musicbrainz_rs::entity::release::Release;
 use musicbrainz_rs::prelude::*;
+use musicbrainz_rs::{MusicBrainzClient, entity::release::Release};
 use std::{io, path::PathBuf, thread, time::Duration};
 
 #[derive(Parser)]
@@ -16,17 +16,28 @@ struct Args {
     inputs: Vec<PathBuf>,
 }
 
+static USER_AGENT: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("CARGO_PKG_REPOSITORY"),
+    " )"
+);
+
 fn main() -> io::Result<()> {
     let args = Args::parse();
     let mut library = FileSystemLibrary::default();
     library.load(&args.inputs)?;
+
+    let mb_client = MusicBrainzClient::new(USER_AGENT);
 
     for artist in &args.artists {
         let releases = Release::browse()
             .by_artist(artist)
             .with_release_groups()
             .limit(100)
-            .execute()
+            .execute_with_client(&mb_client)
             .unwrap();
 
         for release in releases.entities {
