2779da0bb835eaa177fbe0a84000f030a0d15d50
wardxela <wardxela@gmail.com>
2026-09-08 19:49:32 +0300

    nix: update hash

--- package.nix
+++ package.nix
@@ -5,7 +5,7 @@ rustPlatform.buildRustPackage {
 
   name = with builtins; (fromTOML (readFile ./Cargo.toml)).package.name;
 
-  cargoHash = "sha256-Ydtjrm/d9j7x633pZECWp1gawHY6VsdIStB4Q3uBvMM=";
+  cargoHash = "sha256-qe8vX/AaDRs50nrpAyq8ZAIig6lonTVsMFrNK2h5K04=";
 
   meta.mainProgram = "dismus";
 }
